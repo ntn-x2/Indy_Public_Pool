4b86cf9090d0da14d16083d8188449385410169c
Antonio Antonino <contact@antonioantonino.me>
2019-03-19 16:55:25 +0200

    Modify logic for node config. No need to set path to keys folder, but just plain values for verkey, bls_key and pop_bls_key.

--- creator/init_pool.sh
+++ creator/init_pool.sh
@@ -39,6 +39,10 @@ if __name__ == "__main__":
             d_did = d_info["did"]
             d_verkey = d_info["verkey"]
             d_role = d_info["role"]
+
+            if d_role not in {"TRUSTEE", "STEWARD"}:
+                raise Exception("Entity role can only be either \"STEWARD\" or \"TRUSTEE\".")
+
             d_creator = d_info.get("creator", None)
             nym_transaction_payload = {"dest": d_did, "role": "0" if d_role == "TRUSTEE" else "2", "verkey": d_verkey}
             if d_alias is not None:
@@ -46,7 +50,7 @@ if __name__ == "__main__":
             nym_transaction = {"reqSignature": {}, "txn": {"data": nym_transaction_payload, "metadata": {} if d_creator is None else {"from": d_creator}, "type": "1"}, "txnMetadata": {"seqNo": index+1}, "ver": "1"}
             nym_entities_transactions.append(nym_transaction)
         except Exception as ex:
-            print(colored("Syntax error in domain transaction n. {}. Please verify the content of the file. Error: {}".format(index, ex), "red"), file=sys.stderr)
+            print(colored("Syntax error in domain transaction n. {}. Please verify the content of the file. Error: {}".format(index+1, ex), "red"), file=sys.stderr)
             exit(4)
         
     try:
@@ -60,37 +64,22 @@ if __name__ == "__main__":
     for index, p_info in enumerate(pool_info):
         try:
             p_alias = p_info["alias"]
-            p_keys_folder = p_info["keys_folder"]
+            p_verkey = p_info["verkey"]
+            p_bls_key = p_info["bls_key"]
+            p_pop_bls_key = p_info["pop_bls_key"]
             p_ip = p_info["ip"]
-            p_node_port = p_info["node_port"]
-            p_client_port = p_info["client_port"]
+            p_node_port = int(p_info["node_port"])
+            p_client_port = int(p_info["client_port"])
             p_steward_did = p_info["steward_did"]
 
-            keys_folder_path = os.path.realpath(p_keys_folder)
+            node_transaction = {"reqSignature": {}, "txn": {"data": {"data": {"alias": p_alias, "blskey": p_bls_key, "blskey_pop": p_pop_bls_key, "client_ip": p_ip, "client_port": p_client_port, "node_ip": p_ip, "node_port": p_node_port, "services": ["VALIDATOR"]}, "dest": p_verkey}, "type": "0"}, "txnMetadata": {"seqNo": index+1, "txnId": sha256(p_alias.encode()).hexdigest()}, "ver": "1"}
+            if p_steward_did is not None:
+                node_transaction["txn"]["metadata"] = {"from": p_steward_did}
 
-            node_server_keys_friendly_output = os.path.join(keys_folder_path, "keys.out")
-
-            try:
-                with open(node_server_keys_friendly_output, "r") as node_keys:
-                    for (n_index, line) in enumerate(node_keys.read().splitlines()):
-                        if line.startswith("Verification key"):
-                            ver_key = line.split(" ")[3]
-                        elif line.startswith("Public key"):
-                            pub_key = line.split(" ")[3]
-                        elif line.startswith("BLS Public key"):
-                            bls_key = line.split(" ")[4]
-                        elif line.startswith("Proof of possession for BLS key"):
-                            pop_bls_key = line.split(" ")[7]
-                    node_transaction = {"reqSignature": {}, "txn": {"data": {"data": {"alias": p_alias, "blskey": bls_key, "blskey_pop": pop_bls_key, "client_ip": p_ip, "client_port": p_client_port, "node_ip": p_ip, "node_port": p_node_port, "services": ["VALIDATOR"]}, "dest": ver_key}, "type": "0"}, "txnMetadata": {"seqNo": n_index+1, "txnId": sha256(p_alias.encode()).hexdigest()}, "ver": "1"}
-                    if p_steward_did is not None:
-                        node_transaction["txn"]["metadata"] = {"from": p_steward_did}
-                    node_entities_transactions.append(node_transaction)
-            except Exception as ex:
-                print(colored(ex, "red"), file=sys.stderr)
-                exit(6)
+            node_entities_transactions.append(node_transaction)
         except Exception as ex:
             print(colored("Syntax error in pool transaction n. {}. Please verify the content of the file. Error: {}".format(index+1, ex), "red"), file=sys.stderr)
-            exit(7)
+            exit(6)
     
     try:
         with open(domain_transaction_file, "w") as domain_transactions:
@@ -102,4 +91,4 @@ if __name__ == "__main__":
                 pool_transactions.write(json.dumps(node_transaction) + "\n" )
     except Exception as ex:
         print(colored(ex, "red"), file=sys.stderr)
-        exit(8)
+        exit(7)
