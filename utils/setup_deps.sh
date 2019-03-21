df5ec0143d4fbf2248eb635a194dc7e6e6188625
Antonio Antonino <contact@antonioantonino.me>
2019-03-21 10:08:52 +0200

    Modify dependencies commands with sudo execution.

--- utils/setup_deps.sh
+++ utils/setup_deps.sh
@@ -2,11 +2,11 @@
 
 path_folder=`dirname "$0"`
 
-apt update
-apt install -y apt-transport-https python3-pip=8.*
-apt update
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 68DB5E88
-bash -c 'echo "deb https://repo.sovrin.org/deb xenial stable" >> /etc/apt/sources.list'
-apt update || true
-apt install -y indy-node=1.6.83
+sudo apt update
+sudo apt install -y apt-transport-https python3-pip=8.*
+sudo apt update
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 68DB5E88
+sudo bash -c 'echo "deb https://repo.sovrin.org/deb xenial stable" >> /etc/apt/sources.list'
+sudo apt update || true
+sudo apt install -y indy-node=1.6.83
 pip3 install -r .deps.txt
